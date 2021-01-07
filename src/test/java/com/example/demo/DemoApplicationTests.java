188db39b21587b65f1132c085f424c5736b200a8
msmkdxx <chensijia3279@163.com>
2021-01-07 16:03:16 +0800

    解决测试环境问题

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
 import com.alibaba.fastjson.JSON;
+import com.example.demo.entity.Apple;
 import com.google.common.collect.Maps;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
@@ -24,7 +28,7 @@ class DemoApplicationTests {
         Map<String,Object> map = JSON.parseObject(replace);
         System.out.println(map);
         System.out.println(map.get("number"));
-        System.out.println(map.get("commodity_code"));
+
         String[] split = itemCodeUrl.substring(itemCodeUrl.indexOf("?") + 1).split("&");
         Map<String, Object> objectMap = new HashMap<>();
         for (String s : split) {
@@ -41,6 +45,20 @@ class DemoApplicationTests {
 
     @Test
     void testJDK() {
+        List<Apple> appleList = new ArrayList<>();//存放apple对象集合
 
+        Apple apple1 =  new Apple(1,"苹果1",new BigDecimal("3.25"),10);
+        Apple apple12 = new Apple(1,"苹果2",new BigDecimal("1.35"),20);
+        Apple apple2 =  new Apple(2,"香蕉",new BigDecimal("2.89"),30);
+        Apple apple3 =  new Apple(3,"荔枝",new BigDecimal("9.99"),40);
+
+        appleList.add(apple1);
+        appleList.add(apple12);
+        appleList.add(apple2);
+        appleList.add(apple3);
+
+        appleList.stream().forEach(apple -> {
+            System.out.println(apple);
+        });
     }
 }
